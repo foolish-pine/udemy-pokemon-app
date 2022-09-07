dff8345d64def7c6e8b0bd3758a4d14d462f4245
foolish-pine <matsuda.takuya41@gmail.com>
2022-09-07 23:50:36 +0900

    cp13

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
+import { getAllPokemon } from "./utils/pokemon";
+import { getPokemon } from "./utils/pokemon";
 import "./App.css";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon";
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState("true");
+
+  useEffect(() => {
+    const fetchAllPokemonData = async () => {
+      const res = await getAllPokemon(initialURL);
+      loadPokemon(res.results);
+      setLoading(false);
+    };
+    fetchAllPokemonData();
+  }, []);
+
+  const loadPokemon = (data) => {
+    const _pokemoData = Promise.all(
+      data.map((pokemon) => {
+        const pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      })
+    );
+  };
+
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中・・・</h1>
+      ) : (
+        <h1>ポケモンデータを取得しました</h1>
+      )}
+    </div>
+  );
 }
 
 export default App;
